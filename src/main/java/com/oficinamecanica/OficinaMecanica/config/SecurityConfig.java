4595626fc6e81fce0c55bcf60629e66ac17e89c0
elessandroabreu <binho_abreu@hotmail.com>
2025-11-25 14:30:55 -0300

    ATULIZACAO PATCH

--- src/main/java/com/oficinamecanica/OficinaMecanica/config/SecurityConfig.java
+++ src/main/java/com/oficinamecanica/OficinaMecanica/config/SecurityConfig.java
@@ -21,7 +21,6 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-import javax.sound.midi.Patch;
 import java.util.Arrays;
 
 @Configuration
